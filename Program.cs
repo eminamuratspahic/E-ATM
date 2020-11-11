3170f0a9db64e76a131e8ea98a833fa4e5312713
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2020-11-11 14:39:37 +0100

    startup

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace E_ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Emina här");
+        }
+    }
+}
