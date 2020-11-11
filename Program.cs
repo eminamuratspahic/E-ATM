893fd14747f66982754911cde44b7fdba684e4fb
EnesMas <enes_masovic@hotmail.com>
2020-11-11 14:56:25 +0100

    k

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace E_ATM
             Console.WriteLine("Hello World!");
             Console.WriteLine("Emina här");
             Console.WriteLine("Å Enes med");
-            Console.WriteLine("Emina äter");
+            Console.WriteLine("Emina äter...Vad?");
         }
     }
 }
