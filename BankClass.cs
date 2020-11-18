2f45e1d3736455eb12b8f9fd91b28e4180ab44d6
eminamuratspahic <55393321+eminamuratspahic@users.noreply.github.com>
2020-11-18 14:14:41 +0100

    commit changes

--- BankClass.cs
+++ BankClass.cs
@@ -5,7 +5,6 @@ namespace E_ATM
     public class BankClass : IBank
     {
 
-        //public int amountOfWithdrawnsLeft = 5;
         public int maxAmount { get; set; }
         public double amountOfExistingMoney = 10000;
         public double amount { get; set; }
